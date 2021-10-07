9abd9f48b55635a1ba947c063a801d297d3a620a
XxX <XxX@mail.ru>
2021-10-08 01:49:56 +0300

    2.2

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
   <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text>Link One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
+       <v-btn  text><v-icon left>mdi-forum</v-icon>Форум</v-btn> 
+       <v-btn  text><v-icon left>mdi-account-plus</v-icon>Регистрация</v-btn>
+       <v-btn  text><v-icon left>mdi-account-arrow-right</v-icon>Вход</v-btn> 
     </v-toolbar-items>
   </v-app-bar>
-  <v-main>
+  <v-content>
     <router-view></router-view> 
-  </v-main>
+  </v-content>
   </v-app>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
