279bf1aa4ecd3a743324d763d915ee9184dd1c00
XxX <XxX@mail.ru>
2021-10-08 02:29:22 +0300

    2.5

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,61 @@
 <template>
   <v-app>
-  <v-navigation-drawer app v-model="drawer"><v-list-item>
+    <v-navigation-drawer app v-model="drawer" style="background: #7ae3eb" >
+      <v-list-item style="background: #11959e">
         <v-list-item-content>
-          <v-list-item-title class="title">
-            КИПУ
+          <v-list-item-title class="title" style="color: #e8dbdb">
+          КИПУ
           </v-list-item-title>
-          <v-list-item-subtitle>
-            Учебный проект
+          <v-list-item-subtitle style="color: #000000">
+          Учебный проект
           </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item-group color="primary">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-cake-variant</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Link One</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list></v-navigation-drawer> 
-  <v-app-bar app dark color="primary">
+    <v-divider></v-divider>
+    <v-list dense>
+      <v-list-item-group>
+        <v-list-item style="color: #000000" v-for="link in menu" :key="link.title">
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>   
+   </v-navigation-drawer>
+  <v-app-bar app dark style="background: #28c6d1" >
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text><v-icon left>mdi-forum</v-icon>Форум</v-btn> 
-       <v-btn  text><v-icon left>mdi-account-plus</v-icon>Регистрация</v-btn>
-       <v-btn  text><v-icon left>mdi-account-arrow-right</v-icon>Вход</v-btn> 
-    </v-toolbar-items>
-  </v-app-bar>
-  <v-content>
-    <router-view></router-view> 
-  </v-content>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn v-for="link in links" :key="link.title" text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  <v-main style="background: #28d2de" >
+
+  </v-main>
+
+    
   </v-app>
 </template>
-
 <script>
-export default {
+  export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        {title:"Форум", icon:"mdi-forum", url:"/forum"},
+        {title:"Регистрация", icon:"mdi-account-plus", url:"/reg"},
+        {title:"Вход", icon:"mdi-account-arrow-right", url:"/loggin"},
+      ],
+      menu: [
+        {title:"Статистика", icon:"mdi-badge-account-horizontal", url:"/stats"},
+        {title:"Банковские счета", icon:"mdi-bank-outline", url:"/bank"},
+        {title:"Защита аккаунта", icon:"mdi-lock-alert", url:"/lock"},
+        {title:"Мод", icon:"mdi-cellphone-arrow-down", url:"/mod"},
+      ]
+      }
     }
   }
-}
 </script>
