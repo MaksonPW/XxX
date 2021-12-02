ada30bcb48699a9bde967ee708f7e40324559271
XxX <XxX@mail.ru>
2021-12-02 21:15:28 +0300

    ветка 5.1 добавляли текст ..На главную..

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,7 @@
    </v-navigation-drawer>
   <v-app-bar app dark style="background: #28c6d1" >
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-btn text to="/">На главную</v-btn>-b
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn v-for="link in links" :key="link.title" :to="link.url" text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
