8fe08405bbd2976be0846ec88a9e59195370b72d
XxX <XxX@mail.ru>
2021-12-02 20:47:07 +0300

    ветка 3.3

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -4,41 +4,58 @@
       <v-flex xs12 sm8 md8>
         <v-card class="elevetion-12">
           <v-toolbar dark color="primary">
-            Login
+            <v-toolbar-title>Login
+            </v-toolbar-title>
           </v-toolbar>
           <v-card-text>
-            <v-form>
+            <v-form v-model="valid" ref="form"
+                    validation>
               <v-text-field
                   prepend-icon="mdi-account"
                   name="email"
                   label="Email"
                   type="email"
-                v-model="email">
+                  v-model="email"
+                  :rules="emailRules" >
               </v-text-field>
               <v-text-field
                   prepend-icon="mdi-lock"
                   name="password"
                   label="Password"
                   type="password"
-                  v-model="password" >
-              </v-text-field>
+                  v-model="password"
+                  :rules="passwordRules"
+              ></v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary">Login</v-btn>
+            <v-btn color="primary">
+              Login
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
 <script>
+
 export default {
   data () {
     return {
       email: "",
-      password: ""
+      password: "",
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must 					be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length >= 6) || 'Password 					must be more or equel than 6 							characters',
+      ]
     }
   }
 }
